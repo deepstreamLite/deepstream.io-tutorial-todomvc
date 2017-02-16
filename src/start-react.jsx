ed004c0688bfc60b5d00e0d3d7c15dbf537091bc
Yael Stein <yaelstein@Yaels-MacBook-Air.local>
2017-02-16 17:05:34 +0100

    setting up todomvc

--- src/start-react.jsx
+++ src/start-react.jsx
@@ -3,107 +3,34 @@ ds.login({ username: 'ds-simple-input-' + ds.getUid() });
 
 var emitter = new EventEmitter();
 
-var UsersList = React.createClass({
-  getInitialState: function() {
-    return {
-      users: []
-    }
-  },
-  componentDidMount: function() {
-    var list = ds.record.getList( 'users' );
-    list.subscribe(function(entries) {
-      this.setState({users:entries});
-    }.bind(this));
 
-    emitter.addListener('user-added', function( recordName ) {
-      list.addEntry( recordName );
-    });
 
-    emitter.addListener('user-removed', function( recordName ) {
-      list.removeEntry( recordName );
-    });
-  },
-  render: function() {
-    var users = [];
-    for( var i = 0; i < this.state.users.length; i++ ) {
-      users.push( <User recordName={this.state.users[i]}/> );
-    }
-    return (
-      <div>
-        <ul className="users">{users}</ul>
-        <AddUser/>
-      </div>
-    );
-  }
-});
-
-var User = React.createClass({
-  getInitialState: function() {
-    return {
-      isActive: false,
-      firstname: '',
-      lastname: '',
-      title: ''
-    }
-  },
-  componentDidMount: function() {
-    var record = ds.record.getRecord( this.props.recordName );
-    record.subscribe(function(data) {
-      this.setState(data);
-    }.bind(this));
-
-    emitter.addListener('user-selected', function( recordName ) {
-      this.setState({isActive: false});
-    }.bind(this));
-  },
-  render: function() {
-    return (
-      <li className={this.state.isActive ? 'active' : 'bla'}
-          onClick={this.selectUser}>
-        <em>
-          <span>{this.state.firstname}</span>
-          &nbsp;
-          <span>{this.state.lastname}</span>
-        </em>
-        <span>{this.state.title}</span>
-        <div className="delete-button fa fa-remove" onClick={this.deleteUser}></div>
-      </li>
-    );
-  },
-  deleteUser: function() {
-    emitter.emit('user-removed', this.props.recordName);
-  },
-  selectUser: function() {
-    emitter.emit('user-selected', this.props.recordName);
-    this.setState({isActive:true});
-  }
-});
-
-var AddUser = React.createClass({
-  render: function() {
-    return (
-      <div className="addUser" onClick={this.addUser}>
-        <i className="fa fa-plus"></i>
-        <span>add user</span>
-      </div>
-    );
-  },
-  addUser: function() {
-    var recordName = 'users/' + ds.getUid();
-    var record = ds.record.getRecord( recordName );
-    record.set({
-      firstname: 'New',
-      lastname: 'User',
-      title: '-'
-    });
-    emitter.emit('user-added', recordName);
-  }
-});
 
 var UserDetails = React.createClass({
   getInitialState: function() {
-    this.record = ds.record.getAnonymousRecord();
-    return {}
+    var that=this;
+    var todosList = ds.record.getList('todos');
+    todosList.whenReady(()=> {
+      this.todosList = todosList;
+      this.todos = todosList.getEntries();
+      var list = [];
+      var obj = {};
+      for (var i=0;i<this.todos.length;i++) {
+        obj.name=this.todos[i];
+        var rec = ds.record.getRecord(this.todos[i]);
+        rec.whenReady((rec) => {
+          var that=this;
+          obj.title = rec.get('title');
+          obj.isDone = rec.get('isDone');
+          list.push(obj);
+        })
+      }
+this.list = list;
+console.log(list)
+    })
+
+    return {newTodo: ''
+            }
   },
   componentDidMount: function() {
     emitter.addListener('user-selected', function( recordName ) {
@@ -111,66 +38,52 @@ var UserDetails = React.createClass({
     }.bind(this));
   },
   render: function() {
+    console.log(this);
+
     return (
       <div>
-        <fieldset>
-  				<legend>General data</legend>
-  				<ul className="fields">
-            <UserListEntry record={this.record} field='firstname' label='Firstname'/>
-            <UserListEntry record={this.record} field='lastname' label='Lastname'/>
-            <UserListEntry record={this.record} field='title' label='Title'/>
-  				</ul>
-  			</fieldset>
+        <h3>What is there to do</h3>
+        <textArea value={this.state.newTodo} onKeyPress={this.handleKeyPress} onChange={this.handleChange}></textArea>
 
-  			<fieldset>
-  				<legend>Address</legend>
-				    <ul className="fields">
-              <UserListEntry record={this.record} field='street' label='Street'/>
-              <UserListEntry record={this.record} field='number' label='Nr'/>
-              <UserListEntry record={this.record} field='postcode' label='Post Code'/>
-              <UserListEntry record={this.record} field='city' label='City'/>
-            </ul>
-  			</fieldset>
       </div>
     );
-  }
-});
-
-var UserListEntry = React.createClass({
-  getInitialState: function() {
-    return {
-      value: null
+  },
+  handleKeyPress: function(event) {
+    var that=this;
+    var todos = that.todos;
+    var todosList = that.todosList;
+    if(event.key == 'Enter') {
+      console.log('enter pressed');
+      console.log(that.state.newTodo);
+      var id = 'todo/' + ds.getUid();
+      ds.record.getRecord( id ).set({
+        title: that.state.newTodo,
+        isDone: false
+      });
+      console.log(ds.record.getRecord(id))
+      todosList.addEntry(id);
+      console.log(todosList._record._$data);
     }
   },
-  componentDidMount: function() {
-    var record = this.props.record.subscribe(this.props.field, function(newValue) {
-        this.setState({value: newValue});
-    }.bind(this));
-
-    this.setState({value: this.props.record.get(this.props.field)});
-  },
-  render: function() {
-    return (
-      <li>
-        <label>{this.props.label}</label>
-        <input value={this.state.value} onChange={this.onChange} id={this.props.field} type="text" />
-      </li>
-    );
-  },
-  onChange: function(event) {
-    this.setState({value: event.target.value});
-    this.props.record.set(this.props.field, event.target.value);
+  handleChange: function(event) {
+    this.setState({newTodo:event.target.value})
   }
+
 });
+
+
+
+
+
+
+
+
 
 React.render(
   <div id="wrapper">
-		<div className="col left">
-      <UsersList/>
-		</div>
-		<div className="col right">
+    <div className="col right">
       <UserDetails />
-		</div>
-	</div>,
+    </div>
+  </div>,
   document.body
 );
