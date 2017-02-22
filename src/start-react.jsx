b6b5e2a120798555d1d2349695c79a56dc424f9d
Yael Stein <yaelstein@Yaels-Air.fritz.box>
2017-02-22 21:44:23 +0100

    edit option added

--- src/start-react.jsx
+++ src/start-react.jsx
@@ -13,7 +13,8 @@ class ToDo extends React.Component{
     this.state = {
       todos:[],
       newTodo:'',
-      toShow:'all'
+      toShow:'all',
+      counter:0
     }
     this.handleKeyPress = this._handleKeyPress.bind( this );
     this.handleChange = this._handleChange.bind( this );
@@ -21,7 +22,6 @@ class ToDo extends React.Component{
     this.showActive = this._showActive.bind( this );
     this.showCompleted = this._showCompleted.bind( this );
     this.clearCompleted = this._clearCompleted.bind( this );
-
   }
 
   _setEntries( entries ) {
@@ -32,7 +32,6 @@ class ToDo extends React.Component{
 
   render() {
     var todos = this.state.todos;
-    console.log(todos)
     var list = this.list;
     var toShow = this.state.toShow;
     var todos = this.state.todos.map(function(recordName) {
@@ -40,7 +39,6 @@ class ToDo extends React.Component{
         <TodoItem recordName={recordName} list={list} key={recordName} toShow={toShow}/>
       )
     })
-
     return (
       <div>
         <header className="header">
@@ -58,8 +56,7 @@ class ToDo extends React.Component{
           {todos}
         </div>
         <div className="footer">
-          <h4 className="itemsLeft">items left</h4>
-
+          <h4 className="itemsLeft"> {this.state.todos.length} items left</h4>
           <button className="footerButton" onClick={this.showAll}>All</button>
           <button className="footerButton" onClick={this.showActive}>Active</button>
           <button className="footerButton" onClick={this.showCompleted}>Completed</button>
@@ -67,7 +64,6 @@ class ToDo extends React.Component{
         </div>
       </div>
     )
-
   }
 
   _handleKeyPress(e) {
@@ -126,9 +122,6 @@ class ToDo extends React.Component{
 
 
 
-
-
-
 class TodoItem extends React.Component{
   constructor(props) {
     super(props);
@@ -136,20 +129,25 @@ class TodoItem extends React.Component{
     this.record.subscribe(this.setState.bind(this), true);
     this.removeTodo = this._removeTodo.bind( this );
     this.check = this._check.bind( this );
+    this.startEdit = this.startEdit.bind( this );
+    this.handleSubmit = this.handleSubmit.bind( this );
+    this.handleKeyDown = this.handleKeyDown.bind( this );
+    this.handleChange = this.handleChange.bind( this );
+
 
     this.state = {
       title:'',
-      isDone:false
+      isDone:false,
+      editedText:'',
+      toEdit:false
     }
   }
 
   render () {
-    var toShow = this.props.toShow;
-    var isDone = this.state.isDone;
-    var check = this.check;
-    var recordName = this.props.recordName;
-    var title = this.state.title;
-    var removeTodo = this.removeTodo;
+    var {isDone, title, handleEdit, toEdit, editedText} = this.state;
+    var {toShow, recordName} = this.props;
+    var {check, removeTodo, startEdit, handleSubmit, handleChange, handleKeyDown } = this;
+
     function whatToShow() {
       if(toShow=='all' || (isDone==false&&toShow=='active') ||
       (isDone==true&&toShow=='completed') ) {
@@ -160,8 +158,19 @@ class TodoItem extends React.Component{
               type="checkbox"
               checked={isDone}
               onChange={check}/>
-            <label id={recordName}
-              className={isDone ? 'doneTodo' : 'todoText'}>{title}</label>
+              <label id={recordName}
+                className={isDone ? 'doneTodo' : 'todoText'}
+                onDoubleClick={startEdit}
+                >{title}</label>
+              <input
+                ref="editField"
+                className="editInput"
+                className={toEdit ? 'edit' : 'noEdit'}
+                value={editedText}
+                onBlur={handleSubmit}
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                />
             <button className="destroy" onClick={removeTodo} />
           </li>
         )
@@ -173,10 +182,7 @@ class TodoItem extends React.Component{
         {whatToShow()}
       </div>
     )
-
   }
-
-
   _removeTodo() {
     this.props.list.removeEntry( this.record.name );
     this.record.delete();
@@ -197,10 +203,51 @@ class TodoItem extends React.Component{
       })
     }
   }
+
+
+  startEdit() {
+    console.log('double click')
+    console.log(this.refs.editField)
+    this.setState({
+      toEdit: true,
+      editedText: this.state.title
+    });
+  }
+
+  handleChange(event) {
+    console.log(event.target.value)
+    this.setState({editedText: event.target.value});
+  }
+
+  handleSubmit() {
+    var val = this.state.editedText.trim();
+    if( val ) {
+      this.setState({
+        toEdit: false,
+        title: this.state.editedText
+      });
+      var title = this.state.editedText;
+      this.record.set('title', title);
+    }
+    else {
+      this.setState({
+        toEdit: false
+      });
+    }
+  }
+
+  handleKeyDown(event) {
+    var ESCAPE_KEY = 27;
+    var ENTER_KEY = 13;
+    if (event.keyCode === ESCAPE_KEY) {
+      this.setState({
+        toEdit: false
+      });
+    } else if (event.keyCode === ENTER_KEY) {
+      this.handleSubmit();
+    }
+  }
 }
-
-
-
 
 
 
